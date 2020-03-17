a51e7c3f3b879e36c9e2af2510eebbd6e597141f
Vladimir Ivanov <mrcitron@mail.ru>
2020-03-17 22:11:23 +0300

    search added

---
+++ philsApp/app/src/main/java/com/example/philsapp/RVAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.philsapp
+
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class RVAdapter internal constructor(var cards: ArrayList<SearchCard>) :
+    RecyclerView.Adapter<RVAdapter.CardViewHolder>() {
+
+    class CardViewHolder internal constructor(var cardView: CardView) : RecyclerView.ViewHolder(
+        cardView
+    )
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CardViewHolder {
+        val cv = LayoutInflater.from(parent.context)
+            .inflate(R.layout.search_card_view, parent, false) as CardView
+        return CardViewHolder(cv)
+    }
+
+    override fun onBindViewHolder(
+        cardViewHolder: CardViewHolder,
+        position: Int
+    ) {
+        val cardView = cardViewHolder.cardView
+        val title = cardView.findViewById<View>(R.id.title) as TextView
+        title.setText(cards[position].title)
+        val positionInList = cardView.findViewById<View>(R.id.positionInList) as TextView
+        positionInList.setText((position + 1).toString())
+        val content = cardView.findViewById<View>(R.id.type) as TextView
+        content.setText(cards[position].type)
+        val years = cardView.findViewById<View>(R.id.years) as TextView
+        years.setText(cards[position].years)
+        if (cards[position].years == null) {
+            cardView.findViewById<View>(R.id.yearsLabel).visibility = View.INVISIBLE
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return cards.size
+    }
+
+}
--- philsApp/app/src/main/java/com/example/philsapp/SearchTab.kt
+++ philsApp/app/src/main/java/com/example/philsapp/SearchTab.kt
@@ -1,23 +1,48 @@
 package com.example.philsapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class SearchTab : Fragment() {
-
+    private var cards: ArrayList<SearchCard>? = null
+    private var rv: RecyclerView? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_search_tab, container, false)
+        rv = view.findViewById<RecyclerView>(R.id.rv) as RecyclerView
+
+        val llm = LinearLayoutManager(view.context)
+        rv!!.setLayoutManager(llm)
+
+        initializeData()
+        initializeAdapter()
         return view
+    }
+    private fun initializeData() {
+        cards = ArrayList<SearchCard>()
+        (cards as ArrayList<SearchCard>).add(SearchCard("Сократ", "Философ", "1234-5322"))
+        (cards as ArrayList<SearchCard>).add(SearchCard("Марксизм", "Школа", null))
+        (cards as ArrayList<SearchCard>).add(SearchCard("Античность", "Эпоха", null))
+    }
+
+    private fun initializeAdapter() {
+        val adapter = cards?.let { RVAdapter(it) }
+        rv!!.adapter = adapter
     }
 
 }
