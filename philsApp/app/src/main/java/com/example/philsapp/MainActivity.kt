3075f4b60ebe89c28af7a0ca2e5cf8ac7bc7ed87
Vladimir Ivanov <mrcitron@mail.ru>
2020-03-23 20:59:48 +0300

    close search fix

--- philsApp/app/src/main/java/com/example/philsapp/MainActivity.kt
+++ philsApp/app/src/main/java/com/example/philsapp/MainActivity.kt
@@ -267,6 +267,7 @@ class MainActivity : AppCompatActivity() {
         }
         buttonSearchClose.setOnClickListener {
             clearSearchResults()
+            createGraph(false)
         }
         Log.d(TAG,"startQWQW")
         createGraph(false)
